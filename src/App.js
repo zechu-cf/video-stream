cf79cf09de211b0367f2860766b7ad943499400f
Liu Zechu <zechu@cloudflare.com>
2021-07-22 17:13:47 +0800

    use material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { Stream } from "@cloudflare/stream-react";
 import { useState, useEffect } from 'react';
+import { Typography, Button, Card, CardContent, Box, Grid, AppBar, Toolbar } from '@material-ui/core';
 
 function App() {
   const [selectedFile, setSelectedFile] = useState();
   const [isFilePicked, setIsFilePicked] = useState(false);
-
   const [videoIds, setVideoIds] = useState([]);
 
   const loadVideos = async () => {
@@ -21,7 +21,6 @@ function App() {
 
   const updateVideos = () => {
     loadVideos().then((ids) => {
-      console.log("setVideoIds called!");
       setVideoIds(ids);
     });
   }
@@ -31,6 +30,22 @@ function App() {
     setIsFilePicked(true);
   };
 
+  const handleUploadSuccess = (uploadUrl) => {
+    const uid = uploadUrl.split("/").slice(-1)[0];
+    const pollingUrl = `https://video-helper.trackers.ninja/status?uid=${uid}`;
+    var repeatingTask = setInterval(() => {
+      fetch(pollingUrl).then((res) => {
+        res.text().then((isReady) => {
+          if (isReady === 'true') {
+            console.log("Video is ready to stream.");
+            updateVideos();
+            clearInterval(repeatingTask);
+          }
+        });
+      });
+    }, 500);
+  };
+
   const handleSubmission = () => {
     const formData = new FormData();
     formData.append('file', selectedFile);
@@ -38,9 +53,7 @@ function App() {
       .then((response) =>response.text())
       .then((uploadUrl) => {
         console.log("Upload URL is " + uploadUrl);
-        fetch(
-          uploadUrl,
-          {
+        fetch(uploadUrl, {
             method: 'POST',
             body: formData,
           }
@@ -48,19 +61,7 @@ function App() {
           .then((result) => {
             console.log('Success:', result);
             // after success, keep polling every one second until video is ready to stream
-            const uid = uploadUrl.split("/").slice(-1)[0];
-            const pollingUrl = `https://video-helper.trackers.ninja/status?uid=${uid}`;
-            var repeatingTask = setInterval(() => {
-              fetch(pollingUrl).then((res) => {
-                res.text().then((isReady) => {
-                  if (isReady === 'true') {
-                    console.log("isready!!!");
-                    updateVideos();
-                    clearInterval(repeatingTask);
-                  }
-                });
-              });
-            }, 500);
+            handleUploadSuccess(uploadUrl);
           })
           .catch((error) => {
             console.error('Error:', error);
@@ -75,30 +76,70 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Share your video with the community!</h1>
-      <h2>Step one: choose a video file</h2>
-      <div>
-			  <input type="file" name="file" onChange={changeHandler} />
-			  {isFilePicked ? (
-				  <div>
-            <p>Filename: {selectedFile.name}</p>
-            <p>Filetype: {selectedFile.type}</p>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">Cloudshare</Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={1} direction="row">
+        <Grid item xs={3} style={{backgroundColor: '#f5ce42'}}/>
+        <Grid container item xs={6} direction="column" alignItems='center' >
+          <Box m={6}/>
+          <Typography variant="h3">
+          Share your video with the community!
+          </Typography>
+          <Box m={2}/>
+          <Card variant='outlined' style={{width: 400}}>
+            <CardContent>
+              <Typography variant="h5">
+              Step one: choose a video file
+              </Typography>
+              <Button
+                variant="outlined"
+                color="primary"
+                component="label"
+              >
+                <input type="file" name="file" onChange={changeHandler} />
+              </Button>
+              {isFilePicked ? (
+                <div>
+                  <p>Filename: {selectedFile.name}</p>
+                  <p>Filetype: {selectedFile.type}</p>
+                </div>
+              ) : (
+                <p>File must be less than 5 min and smaller than 200MB.</p>
+              )}
+            </CardContent>
+          </Card>
+          <Card variant='outlined' style={{width: 400}}>
+            <CardContent>
+              <Typography variant="h5">
+                Step two: upload it!
+              </Typography>
+              <div>
+                <Button variant="contained" color="primary" onClick={handleSubmission}>
+                  Upload
+                </Button>
+                <p>This process may take up to a minute...</p>
+              </div>
+            </CardContent>
+          </Card>  
+          <Box m={2}/>
+          <Typography variant="h5">
+            Now, let's watch some relaxing videos shared by others!
+          </Typography>
+          <div style={{width: '60%', display: 'inline-block'}}>
+            {
+              videoIds.map((uid) => (
+                <Card style={{margin: 20}}>
+                  <Stream controls src={uid} key={uid}/>
+                </Card>
+              ))
+            }
           </div>
-        ) : (
-          <p>The video file must be less than 5 min and smaller than 200MB.</p>
-        )}
-        <h2>Step two: upload it!</h2>
-        <div>
-          <button onClick={handleSubmission}>Upload</button>
-          <p>This process may take up to a minute.</p>
-        </div>
-      </div>
-      <h1>Now, let's watch some relaxing videos shared by others!</h1>
-      <div style={{width: '30%', display: 'inline-block'}}>
-        {
-          videoIds.map((uid) => (<Stream controls src={uid} key={uid}/>))
-        }
-      </div>
+        </Grid>
+        <Grid item xs={3} style={{backgroundColor: '#f5ce42'}}/>
+      </Grid>
     </div>
   );
 }
